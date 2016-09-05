cb69a463baa54b96fc2f0b687fba251eab3d89cc
trocolit2 <trocolit2@gmail.com>
2016-09-05 16:13:32 -0300

    Implemented first version of evaluation method

--- src/VanishingPointDetectionEvaluation.cpp
+++ src/VanishingPointDetectionEvaluation.cpp
@@ -1,4 +1,8 @@
 #include <VanishingPointDetectionEvaluation.hpp>
+#include <VanishingPointDetectionTools.hpp>
+
+#include <opencv2/highgui/highgui.hpp>
+
 #include <iostream>
 
 namespace vanishing_point {
@@ -43,27 +47,47 @@ std::vector<double> VPDE runEvaluation( VanishingPointDetection detector,
                                         bool show_detection){
 
   std::vector<double> error_vector(_gt_zeniths.size());
-  for(uint i=0; i<error_vector.size(); ++i){
+  for(uint i=0; i < error_vector.size(); ++i){
 
     // load image
     cv::Mat3b image = cv::imread("path");
 
     // detection output;
     std::vector<cv::Point2f> detected_vps;
-    std::vector<int> line_by_vp;
+    std::vector<int> lines_by_vp;
     std::vector<cv::Vec4f> lines_segments;
 
-    detected_vps = detector.applyVPDetector( image, &line_by_vp,
+    detected_vps = detector.applyVPDetector( image, &lines_by_vp,
                                                     &lines_segments );
+    // get zenith point
     cv::Point2f zenith = detected_vps[1];
-    std::vector<cv::Point2f> horizon_points; = detected_vps[1];
+
+    // get others points
+    std::vector<cv::Point2f> horizon_points;
     horizon_points.push_back(detected_vps[0]);
-    for (uint j = 2; j < detected_vps.size(); j++) {
+    for (uint j = 2; j < detected_vps.size(); j++)
       horizon_points.push_back(detected_vps[i]);
-    }
+
+    // count lines per horizontal vanishing_point
+    std::vector<int> numbers_lines_per_vp(detected_vps.size()-1);
+    for(uint k = 0; k < lines_by_vp.size(); k++)
+      if( lines_by_vp[k] != 1)
+        numbers_lines_per_vp[k]++;
+
+    // estimating horizon line
+    cv::Point2f principal_point(image.cols/2, image.rows/2);
+    cv::Point3f horizon_line = horizonLineEstimation( zenith, principal_point,
+                                                      horizon_points,
+                                                      numbers_lines_per_vp);
+
+    // estimating error
+    error_vector[i] = normalizedMaxDistanceBetweenHorizonLines(
+                                                        horizon_line,
+                                                        _gt_horizon_lines[i],
+                                                        image.size());
 
   }
-
+  return error_vector;
 }
 
 
