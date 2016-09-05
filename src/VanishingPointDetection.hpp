c2bcc2ac4b003969df70d86bf8ce22c1d1ada6ae
trocolit2 <trocolit2@gmail.com>
2016-09-05 16:13:00 -0300

    Define the vanishing point detector interface method

--- src/VanishingPointDetection.hpp
+++ src/VanishingPointDetection.hpp
@@ -10,7 +10,7 @@ public:
 
   std::vector<cv::Point2f> applyVPDetector(
                                 cv::Mat image,
-                                std::vector<int> *line_by_vp = 0,
+                                std::vector<int> *line_id_by_vp = 0,
                                 std::vector<cv::Vec4f> *lines_segments = 0 ){
 
     return std::vector<cv::Point2f>();
