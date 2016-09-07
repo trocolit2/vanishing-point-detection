d7828003de50dd741eea665389690d76eafdb94a
trocolit2 <trocolit2@gmail.com>
2016-09-07 17:48:31 -0300

    minor fixes

--- src/line_detectors/aLSD.cpp
+++ src/line_detectors/aLSD.cpp
@@ -1,5 +1,4 @@
 #include <aLSD.hpp>
-
 extern "C" {
   #include <aLSD/lsd.h>
 }
@@ -45,9 +44,6 @@ std::vector<cv::Vec4f> aLSD::applyLSDetector(cv::Mat image){
   for (int i = 0; i < n; i++) {
     cv::Vec4f line_segment(out[7 * i + 0], out[7 * i + 1], // point1
                             out[7 * i + 2], out[7 * i + 3]); // point2
-    // cv::Point2f point1(out[7 * i + 0], out[7 * i + 1]), point2(out[7 * i + 2],
-    //                                                            out[7 * i + 3]);
-    // Line2D line(point1, point2);
     line_segments.push_back(line_segment);
   }
 
