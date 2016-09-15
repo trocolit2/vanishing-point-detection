063265461cb91eaa582ea867413051299c3807b3
trocolit2 <trocolit2@gmail.com>
2016-09-15 11:55:25 -0300

    implemented errorLineSegmentPoint2VP

--- src/vp_detectors/RCSCfMIoMW/Tools.cpp
+++ src/vp_detectors/RCSCfMIoMW/Tools.cpp
@@ -1,4 +1,5 @@
 #include <Tools.hpp>
+#include <VanishingPointDetectionTools.hpp>
 
 #include <iostream>
 
@@ -11,14 +12,29 @@ cv::Point2f lineSegmentCenterPoint(cv::Vec4f line_segment){
 };
 
 double distancePoint2Line(cv::Point3f line, cv::Point2f point){
-  double distance = line.x*point.x+ line.y*point.y + line.z;
+  double distance = line.x*point.x + line.y*point.y + line.z;
   distance = abs(distance)/(sqrt(line.x*line.x + line.y*line.y));
   return distance;
 }
 
 double errorLineSegmentPoint2VP(  cv::Vec4f line_segment,
-                                  cv::Point3f homogeneos_vp){
-  return 0;
+                                  cv::Point3f homogeneos_vp,
+                                  cv::Point2f *center_point,
+                                  cv::Point2f *end_point,
+                                  cv::Point3f *line_center_vp){
+
+  cv::Point2f local_center_point = lineSegmentCenterPoint(line_segment);
+  cv::Point2f vp( homogeneos_vp.x/homogeneos_vp.z,
+                  homogeneos_vp.y/homogeneos_vp.z);
+
+  cv::Point2f local_end_point(line_segment[0],line_segment[1]);
+  cv::Point3f line = defineEuclidianLineBy2Points(local_center_point, vp);
+  if(center_point && end_point && line_center_vp){
+
+  }
+
+
+  return distancePoint2Line(line, local_end_point);
 }
 
 }
--- src/vp_detectors/RCSCfMIoMW/Tools.hpp
+++ src/vp_detectors/RCSCfMIoMW/Tools.hpp
@@ -7,6 +7,9 @@ cv::Point2f lineSegmentCenterPoint(cv::Vec4f line_segment);
 double distancePoint2Line(cv::Point3f line, cv::Point2f point);
 
 double errorLineSegmentPoint2VP(  cv::Vec4f line_segment,
-                                  cv::Point3f homogeneos_vp);
+                                  cv::Point3f homogeneos_vp,
+                                  cv::Point2f *center_point = 0,
+                                  cv::Point2f *end_point = 0,
+                                  cv::Point3f *line_center_vp = 0);
 
 }
