5928a0d3542a1918624f1bb28ddf8aa14df4d90f
trocolit2 <trocolit2@gmail.com>
2016-09-28 18:59:55 -0300

    Implemented tests to consensus set

--- test/VPDetector_RCSCfMIoMW_Tools_test.cpp
+++ test/VPDetector_RCSCfMIoMW_Tools_test.cpp
@@ -398,54 +398,55 @@ BOOST_AUTO_TEST_CASE(filterHypotheses_testCase){
   }
 }
 
-// BOOST_AUTO_TEST_CASE(consensusSet_testCase){
-//
-//   cv::Mat3b image = cv::Mat3b::zeros(10,10);
-//   cv::Point2f center_point(image.cols/2, image.rows/2);
-//   std::vector<cv::Vec4f> segments = {  cv::Vec4f(0.4, 0, 0.45, 0.05),
-//           cv::Vec4f(0.6, 0.05, 0.65, 0),    cv::Vec4f(0.15, 0.1, 0.2, 0.1),
-//           cv::Vec4f(0.8, 0.1, 0.85, 0.1),   cv::Vec4f(0.45, 0.2, 0.5, 0.15),
-//           cv::Vec4f(0.65, 0.15, 0.7, 0.2),  cv::Vec4f(0.15, 0.3, 0.2, 0.35),
-//           cv::Vec4f(0.05, 0.5, 0.1, 0.5),   cv::Vec4f(0.4, 0.45, 0.45, 0.4),
-//           cv::Vec4f(0.6, 0.5, 0.65, 0.5),   cv::Vec4f(0.35, 0.6, 0.35, 0.65),
-//           cv::Vec4f(0.6, 0.75, 0.65, 0.8),  cv::Vec4f(0.9, 0, 0.9, 0.05),
-//           cv::Vec4f(0.8, 0.5, 0.85, 0.55),  cv::Vec4f(0.95, 0.55, 1.0, 0.5),
-//           cv::Vec4f(0.95, 0.65, 1.0, 0.7), cv::Vec4f(0.75, 0.75, 0.8, 0.7),
-//           cv::Vec4f(0.9, 0.95, 0.9, 1.0)};
-//
-//   std::vector<cv::Point2f> vps = {cv::Point2f(0.55, 0.1), cv::Point2f(0.9, 0.6),
-//                                   cv::Point2f(0.35, 0.5)};
-//
-//   // cv::Mat(segments) = cv::Mat(segments)*image.cols;
-//   for (uint i = 0; i < segments.size(); i++) {
-//     double error = errorLineSegmentPoint2VP(segments[i],
-//                                         cv::Point3f(vps[0].x,
-//                                            vps[0].y, 1));
-//     std::cout<<"I " << i << " ERROR " << error << std::endl;
-//     std::cout<<"\n";
-//   }
-//
-//   // std::vector<int> lines_cluster;
-//   // uint count = consensusSet(vps, segments, lines_cluster, 0.0001);
-//   // std::cout<<cv::Mat(lines_cluster)<<std::endl;
-//
-//   // for (uint i = 0; i < segments.size(); i++) {
-//   //   cv::Scalar color (255, 255, 255);
-//   //   if(lines_cluster[i] == 0){
-//   //     color =  cv::Scalar(255, 0, 0);
-//   //   }else if(lines_cluster[i] == 1){
-//   //     color =  cv::Scalar(0, 255, 0);
-//   //   }else if(lines_cluster[i] == 2){
-//   //     color =  cv::Scalar(0, 0, 255);
-//   //   }
-//   //   cv::line( image,
-//   //           cv::Point2f(segments[i][0]*image.cols, segments[i][1]*image.rows),
-//   //           cv::Point2f(segments[i][2]*image.cols, segments[i][3]*image.rows),
-//   //           color, image.rows * 0.008);
-//   // }
-//   // cv::imshow("out", image);
-//   // cv::waitKey();
-// }
+BOOST_AUTO_TEST_CASE(consensusSet_testCase){
+
+  cv::Mat3b image = cv::Mat3b::zeros(1000,1000);
+  cv::Point2f center_point(image.cols/2, image.rows/2);
+  std::vector<cv::Vec4f> segments = {  cv::Vec4f(0.4, 0, 0.45, 0.05),
+          cv::Vec4f(0.6, 0.05, 0.65, 0),    cv::Vec4f(0.15, 0.1, 0.2, 0.1),
+          cv::Vec4f(0.8, 0.1, 0.85, 0.1),   cv::Vec4f(0.45, 0.2, 0.5, 0.15),
+          cv::Vec4f(0.65, 0.15, 0.7, 0.2),  cv::Vec4f(0.15, 0.3, 0.2, 0.35),
+          cv::Vec4f(0.05, 0.5, 0.1, 0.5),   cv::Vec4f(0.4, 0.45, 0.45, 0.4),
+          cv::Vec4f(0.6, 0.5, 0.65, 0.5),   cv::Vec4f(0.35, 0.6, 0.35, 0.65),
+          cv::Vec4f(0.6, 0.75, 0.65, 0.8),  cv::Vec4f(0.9, 0, 0.9, 0.05),
+          cv::Vec4f(0.8, 0.5, 0.85, 0.55),  cv::Vec4f(0.95, 0.55, 1.0, 0.5),
+          cv::Vec4f(0.95, 0.65, 1.0, 0.7), cv::Vec4f(0.75, 0.75, 0.8, 0.7),
+          cv::Vec4f(0.9, 0.95, 0.9, 1.0)};
+
+  std::vector<cv::Point2f> vps = {cv::Point2f(0.55, 0.1), cv::Point2f(0.9, 0.6),
+                                  cv::Point2f(0.35, 0.5)};
+
+  std::vector<int> gt_cluster = {-1, 0, 0, 0, 0, -1, 2, 2, 2, 2, 2,
+                                  2, 1, 1, 1, 1, 1, 1};
+
+  std::vector<int> lines_cluster;
+  uint count = consensusSet(vps, segments, lines_cluster, 0.0001);
+
+  for (uint i = 0; i < lines_cluster.size(); i++)
+    BOOST_CHECK_EQUAL(gt_cluster[i], lines_cluster[i]);
+
+  // DEBUG
+  // std::cout<<cv::Mat(lines_cluster).t()<<std::endl;
+  // for (uint i = 0; i < segments.size(); i++) {
+  //   cv::Scalar color (255, 255, 255);
+  //   if(lines_cluster[i] == 0){
+  //     color =  cv::Scalar(255, 0, 0);
+  //   }else if(lines_cluster[i] == 1){
+  //     color =  cv::Scalar(0, 255, 0);
+  //   }else if(lines_cluster[i] == 2){
+  //     color =  cv::Scalar(0, 0, 255);
+  //   }
+  //   cv::line( image,
+  //           cv::Point2f(segments[i][0]*image.cols, segments[i][1]*image.rows),
+  //           cv::Point2f(segments[i][2]*image.cols, segments[i][3]*image.rows),
+  //           color, image.rows * 0.004);
+  // }
+  // for (uint i = 0; i < vps.size(); i++)
+  //   cv::circle( image, vps[i] * image.cols, image.rows * 0.004, cv::Scalar(255,255), -1);
+  //   cv::imshow("out", image);
+  //
+  // cv::waitKey();
+}
 
 
 
