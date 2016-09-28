972b1c01a06323e7a8482a48e7252d955ed79935
trocolit2 <trocolit2@gmail.com>
2016-09-28 18:26:35 -0300

    Adjust tests from the new precision results from distance line to point

--- test/VPDetector_RCSCfMIoMW_Tools_test.cpp
+++ test/VPDetector_RCSCfMIoMW_Tools_test.cpp
@@ -68,7 +68,7 @@ BOOST_AUTO_TEST_CASE(distancePoint2Line_testCase){
 
 BOOST_AUTO_TEST_CASE(errorLineSegmentPoint2VP_testCase){
 
-  std::vector<double> gt_error = {10, 5, 6, 100, 3, 9};
+  std::vector<double> gt_error = {10, 5, 6, 100, 3.02, 9.0673};
 
   cv::Point2f center(250, 250);
   std::vector<cv::Vec4f> line_segments = {
@@ -330,11 +330,11 @@ BOOST_AUTO_TEST_CASE(filterHypotheses_testCase){
               cv::Vec4f(0.1, 0.7, 0.1, 0.8), cv::Vec4f(0.2, 0.9, 0.3, 0.9),
               cv::Vec4f(0.98, 1.0, 1.0, 1.0)};
 
-  std::vector< std::vector<int> > gt_foco(4);
+  std::vector< std::vector<double> > gt_foco(4);
   gt_foco[0] = {220, 170, 51};
-  gt_foco[1] = {152};
-  gt_foco[2] = {3962, 1857, 50322, 349, 398};
-  gt_foco[3] = {212};
+  gt_foco[1] = {152, 0.1159};
+  gt_foco[2] = {3962, 225.55959924707261, 1857, 50322, 349, 398};
+  gt_foco[3] = {200, 212};
 
 
   for (uint k = 0; k < set4_segments.size(); k++) {
@@ -360,12 +360,11 @@ BOOST_AUTO_TEST_CASE(filterHypotheses_testCase){
     std::vector< double > vector_foco;
     std::vector< std::vector<cv::Point2f> > vector_vps;
     vector_vps = estimationVPby4LinesInAll9Cases(lines, &vector_foco);
-
     vector_vps = filterHypotheses(vector_vps, vector_foco, line_segments);
 
 
     for (uint i = 0; i < vector_foco.size(); i++) {
-      BOOST_CHECK_EQUAL(gt_foco[k][i], (int) vector_foco[i]);
+      BOOST_CHECK_CLOSE(gt_foco[k][i], vector_foco[i], 1.0);
     }
 
 
@@ -399,9 +398,54 @@ BOOST_AUTO_TEST_CASE(filterHypotheses_testCase){
   }
 }
 
-BOOST_AUTO_TEST_CASE(consensusSet_testCase){
-
-}
+// BOOST_AUTO_TEST_CASE(consensusSet_testCase){
+//
+//   cv::Mat3b image = cv::Mat3b::zeros(10,10);
+//   cv::Point2f center_point(image.cols/2, image.rows/2);
+//   std::vector<cv::Vec4f> segments = {  cv::Vec4f(0.4, 0, 0.45, 0.05),
+//           cv::Vec4f(0.6, 0.05, 0.65, 0),    cv::Vec4f(0.15, 0.1, 0.2, 0.1),
+//           cv::Vec4f(0.8, 0.1, 0.85, 0.1),   cv::Vec4f(0.45, 0.2, 0.5, 0.15),
+//           cv::Vec4f(0.65, 0.15, 0.7, 0.2),  cv::Vec4f(0.15, 0.3, 0.2, 0.35),
+//           cv::Vec4f(0.05, 0.5, 0.1, 0.5),   cv::Vec4f(0.4, 0.45, 0.45, 0.4),
+//           cv::Vec4f(0.6, 0.5, 0.65, 0.5),   cv::Vec4f(0.35, 0.6, 0.35, 0.65),
+//           cv::Vec4f(0.6, 0.75, 0.65, 0.8),  cv::Vec4f(0.9, 0, 0.9, 0.05),
+//           cv::Vec4f(0.8, 0.5, 0.85, 0.55),  cv::Vec4f(0.95, 0.55, 1.0, 0.5),
+//           cv::Vec4f(0.95, 0.65, 1.0, 0.7), cv::Vec4f(0.75, 0.75, 0.8, 0.7),
+//           cv::Vec4f(0.9, 0.95, 0.9, 1.0)};
+//
+//   std::vector<cv::Point2f> vps = {cv::Point2f(0.55, 0.1), cv::Point2f(0.9, 0.6),
+//                                   cv::Point2f(0.35, 0.5)};
+//
+//   // cv::Mat(segments) = cv::Mat(segments)*image.cols;
+//   for (uint i = 0; i < segments.size(); i++) {
+//     double error = errorLineSegmentPoint2VP(segments[i],
+//                                         cv::Point3f(vps[0].x,
+//                                            vps[0].y, 1));
+//     std::cout<<"I " << i << " ERROR " << error << std::endl;
+//     std::cout<<"\n";
+//   }
+//
+//   // std::vector<int> lines_cluster;
+//   // uint count = consensusSet(vps, segments, lines_cluster, 0.0001);
+//   // std::cout<<cv::Mat(lines_cluster)<<std::endl;
+//
+//   // for (uint i = 0; i < segments.size(); i++) {
+//   //   cv::Scalar color (255, 255, 255);
+//   //   if(lines_cluster[i] == 0){
+//   //     color =  cv::Scalar(255, 0, 0);
+//   //   }else if(lines_cluster[i] == 1){
+//   //     color =  cv::Scalar(0, 255, 0);
+//   //   }else if(lines_cluster[i] == 2){
+//   //     color =  cv::Scalar(0, 0, 255);
+//   //   }
+//   //   cv::line( image,
+//   //           cv::Point2f(segments[i][0]*image.cols, segments[i][1]*image.rows),
+//   //           cv::Point2f(segments[i][2]*image.cols, segments[i][3]*image.rows),
+//   //           color, image.rows * 0.008);
+//   // }
+//   // cv::imshow("out", image);
+//   // cv::waitKey();
+// }
 
 
 
