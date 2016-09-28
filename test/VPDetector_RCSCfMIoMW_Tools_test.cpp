f83adf81ba185710fa2290d0b8848f785367dfe9
trocolit2 <trocolit2@gmail.com>
2016-09-28 11:33:12 -0300

    Initial consensus set testes

--- test/VPDetector_RCSCfMIoMW_Tools_test.cpp
+++ test/VPDetector_RCSCfMIoMW_Tools_test.cpp
@@ -399,32 +399,39 @@ BOOST_AUTO_TEST_CASE(filterHypotheses_testCase){
   }
 }
 
-BOOST_AUTO_TEST_CASE(RANSAC_testCase){
-
-  cv::Mat3b image = cv::Mat3b::zeros(500,500);
-  cv::Point2f center_point(image.cols/2, image.rows/2);
-  std::vector<cv::Vec4f> line_segments = {
-              cv::Vec4f(0.6, 0.1, 0.7, 0.1), cv::Vec4f(0.55, 0.2, 0.55, 0.3),
-              cv::Vec4f(0.7, 0.4, 0.8, 0.5), cv::Vec4f(0.4, 0.6, 0.6, 0.7),
-              cv::Vec4f(0.1, 0.8, 0.245, 0.534)};
-
-
-  for (uint i = 0; i < line_segments.size(); i++) {
-    cv::Mat temp_mat = cv::Mat(line_segments).row(i);
-    for (uint j = 0; j < 2; j++) {
-      cv::Mat1f local_mat(temp_mat);
-      local_mat[0][j*2] = local_mat[0][j*2]*image.cols - center_point.x;
-      local_mat[0][j*2+1] = local_mat[0][j*2+1]*image.rows - center_point.y;
-    }
-  }
-
-  std::vector<cv::Point3f> lines;
-  for (uint i = 0; i < line_segments.size()-1; i++) {
-    cv::Point2f initial_point(line_segments[i][0], line_segments[i][1]);
-    cv::Point2f end_point(line_segments[i][2], line_segments[i][3]);
-    lines.push_back(defineEuclidianLineBy2Points(initial_point, end_point));
-  }
-
-  RANSAC(line_segments, lines, 10);
+BOOST_AUTO_TEST_CASE(consensusSet_testCase){
 
 }
+
+
+
+
+// BOOST_AUTO_TEST_CASE(RANSAC_testCase){
+//
+//   cv::Mat3b image = cv::Mat3b::zeros(500,500);
+//   cv::Point2f center_point(image.cols/2, image.rows/2);
+//   std::vector<cv::Vec4f> line_segments = {
+//               cv::Vec4f(0.6, 0.1, 0.7, 0.1), cv::Vec4f(0.55, 0.2, 0.55, 0.3),
+//               cv::Vec4f(0.7, 0.4, 0.8, 0.5), cv::Vec4f(0.4, 0.6, 0.6, 0.7),
+//               cv::Vec4f(0.1, 0.8, 0.245, 0.534)};
+//
+//
+//   for (uint i = 0; i < line_segments.size(); i++) {
+//     cv::Mat temp_mat = cv::Mat(line_segments).row(i);
+//     for (uint j = 0; j < 2; j++) {
+//       cv::Mat1f local_mat(temp_mat);
+//       local_mat[0][j*2] = local_mat[0][j*2]*image.cols - center_point.x;
+//       local_mat[0][j*2+1] = local_mat[0][j*2+1]*image.rows - center_point.y;
+//     }
+//   }
+//
+//   std::vector<cv::Point3f> lines;
+//   for (uint i = 0; i < line_segments.size()-1; i++) {
+//     cv::Point2f initial_point(line_segments[i][0], line_segments[i][1]);
+//     cv::Point2f end_point(line_segments[i][2], line_segments[i][3]);
+//     lines.push_back(defineEuclidianLineBy2Points(initial_point, end_point));
+//   }
+//
+//   RANSAC(line_segments, lines, 10);
+//
+// }
